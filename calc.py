bf16f3e6620553adc044d20269946b749cd3e642
dvempati <dvempati@purdue.edu>
2015-10-08 08:52:30 -0400

    added exponent

--- calc.py
+++ calc.py
@@ -33,6 +33,11 @@ def mult(a, b):
 def div(a, b):
     return a / b
 
+#Exponent function
+#a --base
+#b --exponent
+def exp(a,b):
+    return a**b
 
 # -------------------------------------------------------- #
 
@@ -67,8 +72,11 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
+        elif (op == "**"):
+            print "Exponent: ",exp(a,b)
         else:
             print "Invalid operation..."
+
 
     q = raw_input("Quit? [y/n] ")
     if (q == "y" or q == "Y"):
